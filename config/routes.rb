510eb368317080684cf758a7f04775c87afff6d3
nn9637 <wd.37nn@gmail.com>
2020-07-23 20:42:38 +0900

    deviseに関するビューファイルの配置

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'massages/index'
+  # devise_for :users
+  get 'messages/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root "massages#index"
+  root "messages#index"
 end
